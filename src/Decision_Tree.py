c9c23bbe88011fdf033cbe9ca826fc922a112af3
ShaotingZhang <saicomb1990@gmail.com>
2019-05-28 23:21:02 -0500

    add two code samples

---
+++ src/Decision_Tree.py
@@ -0,0 +1,249 @@
+import os
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+from sklearn.model_selection import cross_val_score
+from sklearn.base import BaseEstimator
+from sklearn.metrics import accuracy_score
+from sklearn.model_selection import train_test_split
+
+class TreeNode:
+    def __init__(self, is_leaf, prediction, split_feature):
+        self.is_leaf = is_leaf
+        self.prediction = prediction
+        self.split_feature = split_feature
+        self.left = None
+        self.right = None
+
+class WeightedDecisionTree(BaseEstimator):
+    def __init__(self, max_depth, min_error):
+        self.max_depth = max_depth
+        self.min_error = min_error
+
+    def fit(self, X, Y, data_weights=None):
+        data_set = pd.concat([X, Y], axis=1)
+        features = X.columns
+        target = Y.columns[0]
+        self.root_node = create_weighted_tree(data_set, data_weights, features, target, current_depth=0, max_depth=self.max_depth, min_error=self.min_error)
+
+    def predict(self, X):
+        prediction = X.apply(lambda row: predict_single_data(self.root_node, row), axis=1)
+        return prediction
+
+    def score(self, testX, testY):
+        target = testY.columns[0]
+        result = self.predict(testX)
+        return accuracy_score(testY[target], result)
+
+class MyAdaboost(BaseEstimator):
+    def __init__(self, M):
+        self.M = M
+
+    def fit(self, X, Y):
+        self.models = []
+        self.model_weights = []
+        self.target = Y.columns[0]
+
+        N, _ = X.shape
+        alpha = np.ones(N) / N
+
+        for m in range(self.M):
+            tree = WeightedDecisionTree(max_depth=2, min_error=1e-15)
+            tree.fit(X, Y, data_weights=alpha)
+            prediction = tree.predict(X)
+
+            weighted_error = alpha.dot(prediction != Y[self.target])
+
+            model_weight = 0.5 * (np.log(1 - weighted_error) - np.log(weighted_error))
+            
+            alpha = alpha * np.exp(-model_weight * Y[self.target] * prediction)
+            alpha = alpha / alpha.sum()
+
+            self.models.append(tree)
+            self.model_weights.append(model_weight)
+
+    def predict(self, X):
+        N, _ = X.shape
+        result = np.zeros(N)
+        for wt, tree in zip(self.model_weights, self.models):
+            result += wt * tree.predict(X)
+        return np.sign(result)
+
+    def score(self, testX, testY):
+        result = self.predict(testX)
+        return accuracy_score(testY[self.target], result)
+
+
+def dummies(data, columns=['pclass', 'name_title', 'embarked', 'sex']):
+    """
+    add the column to the data
+    """
+    for col in columns:
+        data[col] = data[col].apply(lambda x: str(x))
+        new_cols = [col + '_' + i for i in data[col].unique()]
+        data = pd.concat([data, pd.get_dummies(data[col], prefix=col)[new_cols]], axis=1)
+        del data[col]
+    return data
+
+
+def node_weighted_mistakes(targets_in_node, data_weights):
+    weight_positive = sum(data_weights[targets_in_node == +1])
+    weighted_mistakes_negative = weight_positive 
+
+    weight_negative = sum(data_weights[targets_in_node == -1])
+    weighted_mistakes_positive = weight_negative
+
+    if weighted_mistakes_negative < weighted_mistakes_positive:
+        return (weighted_mistakes_negative, -1)
+    else:
+        return (weighted_mistakes_positive, + 1)
+
+
+def best_split_weighted(data, features, target, data_weights):
+    # return the best feature
+    best_feature = None
+    best_error = float("inf")
+    num_data_points = float(len(data))
+
+    for feature in features:
+        left_split = data[data[feature] == 0]
+        left_data_weights = data_weights[data[feature] == 0]
+
+        right_split = data[data[feature] == 1]
+        right_data_weights = data_weights[data[feature] == 1]
+
+
+        left_misses, left_class = node_weighted_mistakes(left_split[target], left_data_weights)
+        right_misses, right_class = node_weighted_mistakes(right_split[target], right_data_weights)
+        
+        error = (left_misses + right_misses) * 1.0 / sum(data_weights)
+
+        if error < best_error:
+            best_error = error
+            best_feature = feature
+    return best_feature
+
+def create_leaf(target_values, data_weights):
+    leaf = TreeNode(True, None, None)
+    weighted_error, prediction_class = node_weighted_mistakes(target_values, data_weights)
+    leaf.prediction = prediction_class
+    return leaf
+
+
+def create_weighted_tree(data, data_weights, features, target, current_depth=0, max_depth=10, min_error=1e-15):
+    remaining_features = features[:]
+    target_values = data[target]
+
+    if node_weighted_mistakes(target_values, data_weights)[0] <= min_error:
+        print("Termination 1 reached.")
+        return create_leaf(target_values, data_weights)
+
+    if len(remaining_features) == 0:
+        print("Termination 2 reached.")
+        return create_leaf(target_values, data_weights)
+
+    if current_depth >= max_depth:
+        print("Termination 3 reached.")
+        return create_leaf(target_values, data_weights)
+
+    split_feature = best_split_weighted(data, features, target, data_weights)
+    left_split = data[data[split_feature] == 0]
+    right_split = data[data[split_feature] == 1]
+
+    left_data_weights = data_weights[data[split_feature] == 0]
+    right_data_weights = data_weights[data[split_feature] == 1]
+
+    remaining_features = remaining_features.drop(split_feature)
+    print("Split on feature %s. (%s, %s)" % (split_feature, str(len(left_split)), str(len(right_split))))
+
+    if len(left_split) == len(data):
+        print("Perfect split!")
+        return create_leaf(left_split[target], left_data_weights)
+    if len(right_split) == len(data):
+        print("Perfect split!")
+        return create_leaf(right_split[target], right_data_weights)
+
+    left_tree = create_weighted_tree(left_split, left_data_weights, remaining_features, target, current_depth + 1, max_depth, min_error)
+    right_tree = create_weighted_tree(right_split, right_data_weights, remaining_features, target, current_depth + 1, max_depth, min_error)
+
+    result_node = TreeNode(False, None, split_feature)
+    result_node.left = left_tree
+    result_node.right = right_tree
+    return result_node
+
+
+def count_leaves(tree):
+    if tree.is_leaf:
+        return 1
+    return count_leaves(tree.left) + count_leaves(tree.right)
+
+
+def predict_single_data(tree, x, annotate=False):
+    if tree.is_leaf:
+        if annotate:
+            print("leaf node, predicting %s" % tree.prediction)
+        return tree.prediction
+    else:
+        split_feature_value = x[tree.split_feature]
+
+        if annotate:
+            print("Split on %s = %s" % (tree.split_feature, split_feature_value))
+        if split_feature_value == 0:
+            return predict_single_data(tree.left, x, annotate)
+        else:
+            return predict_single_data(tree.right, x, annotate)
+
+
+def evaluate_accuracy(tree, data):
+    prediction = data.apply(lambda row: predict_single_data(tree, row), axis=1)
+    accuracy = (prediction == data['class']).sum() * 1.0 / len(data)
+    return accuracy
+
+
+def main():
+    np.random.seed(9)
+
+    data_folder = "../input"
+    data = pd.read_csv(os.path.join(data_folder, "mushrooms.csv"))
+
+    "process the input data"
+    target = 'class'
+    data[target] = data.apply(lambda row: -1 if row[0] == 'e' else 1, axis=1)
+    cols = data.columns.drop(target)
+    data_set = dummies(data, columns=cols)
+
+    "split the input data into training data and testing data"
+    train_data, test_data = train_test_split(data_set, test_size=0.3)
+
+    trainX, trainY = train_data[train_data.columns[1:]], pd.DataFrame(train_data[target])
+    testX, testY = test_data[test_data.columns[1:]], pd.DataFrame(test_data[target])
+
+    #test
+    example_targets = np.array([-1, -1, 1, 1, 1])
+    example_data_weights = np.array([1., 2., .5, 1., 1.])
+    node_weighted_mistakes(example_targets, example_data_weights)
+    # (2.5, -1)
+
+    # test
+    features = data_set.columns.drop(target)
+    example_data_weights = np.array(len(train_data) * [2])
+    best_split_weighted(train_data, features, target, example_data_weights)
+    # ('odor_n')
+
+    # test
+    example_data_weights = np.array([1.0 for i in range(len(train_data))])
+    small_data_decision_tree = create_weighted_tree(train_data, example_data_weights, features, target, max_depth=2)
+    count_leaves(small_data_decision_tree)
+
+
+    evaluate_accuracy(small_data_decision_tree, test_data)
+
+    m = MyAdaboost(10)
+
+    m.fit(trainX, trainY)
+
+    m.score(testX, testY)
+
+if __name__ == '__main__':
+    main()
---
+++ src/Dogs-and-cats.py
@@ -0,0 +1,126 @@
+import os
+import cv2          # pip3 install opencv-python
+import numpy as np
+import tflearn
+import matplotlib.pyplot as plt
+import tensorflow as tf
+
+from tflearn.layers.conv import conv_2d, max_pool_2d
+from tflearn.layers.core import input_data, dropout, fully_connected
+from tflearn.layers.estimator import regression
+from random import shuffle
+from tqdm import tqdm
+
+
+def label_img(img):
+    word_label = img.split('.')[-3]
+    if word_label == 'cat':
+        return [1, 0]
+    elif word_label == 'dog':
+        return [0, 1]
+
+def create_train_data(train_dir, img_size):
+    training_data = []
+    for img in tqdm(os.listdir(train_dir)):
+        if not img.endswith('.jpg'):
+            continue
+        label = label_img(img)
+        path = os.path.join(train_dir, img)
+        img = cv2.imread(path, cv2.IMREAD_GRAYSCALE)
+        img = cv2.resize(img, (img_size, img_size))
+        training_data.append([np.array(img), np.array(label)])
+    shuffle(training_data)
+    return training_data
+
+def process_test_data(test_dir, img_size):
+    testing_data = []
+    for img in tqdm(os.listdir(test_dir)):
+        if not img.endswith('.jpg'):
+            continue
+        path = os.path.join(test_dir, img)
+        imgnum = img.split('.')[0]
+        img = cv2.imread(path, cv2.IMREAD_GRAYSCALE)
+        img = cv2.resize(img, (img_size, img_size))
+        testing_data.append([np.array(img), imgnum])
+    shuffle(testing_data)
+    return testing_data
+
+def Model_generation(train_dir, img_size, lr):
+    train_data = create_train_data(train_dir, img_size)
+    tf.reset_default_graph()        # empty the graph
+    convnet = input_data(shape=[None, img_size, img_size, 1], name='input')
+
+    # Three CNN layers and two max pooling layers
+    convnet = conv_2d(convnet, 32, 5, activation='relu')
+    convnet = max_pool_2d(convnet, 5)
+
+    convnet = conv_2d(convnet, 64, 5, activation='relu')
+    convnet = max_pool_2d(convnet, 5)
+
+    convnet = conv_2d(convnet, 128, 5, activation='relu')
+    convnet = max_pool_2d(convnet, 5)
+
+    convnet = conv_2d(convnet, 64, 5, activation='relu')
+    convnet = max_pool_2d(convnet, 5)
+
+    convnet = conv_2d(convnet, 32, 5, activation='relu')
+    convnet = max_pool_2d(convnet, 5)
+
+    # Two fully connected layer and prediction layer
+    convnet = fully_connected(convnet, 1024, activation='relu')
+    convnet = dropout(convnet, 0.8)
+
+    convnet = fully_connected(convnet, 2, activation='softmax')
+    convnet = regression(convnet, optimizer='adam', learning_rate=lr, loss='categorical_crossentropy', name='targets')
+
+    # split the training data set
+    model = tflearn.DNN(convnet, tensorboard_dir='log')
+    train_now = train_data[:-500]
+    test_now = train_data[-500:]
+
+    train_in = np.array([i[0] for i in train_now], dtype=np.float64).reshape(-1, img_size, img_size, 1)
+    train_out = np.array([i[1] for i in train_now], dtype=np.float64)
+    test_in = np.array([i[0] for i in test_now], dtype=np.float64).reshape(-1, img_size, img_size, 1)
+    test_out = np.array([i[1] for i in test_now], dtype=np.float64)
+
+    # Train data in the training set
+    model.fit({'input': train_in}, {'targets': train_out}, n_epoch=3, validation_set=({'input': test_in}, {'targets': test_out}), snapshot_step=500, show_metric=True, run_id='model')
+    return model
+
+
+def main():
+    train_dir = '../input/train/'
+    test_dir = '../input/test/'
+    img_size = 50
+    lr = 1e-3
+    
+    # Generate the model
+    model = Model_generation(train_dir, img_size, lr)
+
+    # Read the testing set
+    test_data = process_test_data(test_dir, img_size)
+
+    # Predict some example in testing set
+    fig = plt.figure()
+    for num, data in enumerate(test_data[:16]):
+        img_num = data[1]
+        img_data = data[0]
+        output = fig.add_subplot(4, 4, num+1)
+        orig = img_data
+        data = img_data.reshape(img_size, img_size, 1)
+        model_out = model.predict([data])[0]
+        if np.argmax(model_out) == 1:
+            label = 'Dog'
+        else:
+            label = 'Cat'
+
+        output.imshow(orig, cmap='gray')
+        plt.title(label)
+        output.axes.get_xaxis().set_visible(False)
+        output.axes.get_yaxis().set_visible(False)
+
+    plt.tight_layout()
+    plt.show()
+
+if __name__ == '__main__':
+    main()
